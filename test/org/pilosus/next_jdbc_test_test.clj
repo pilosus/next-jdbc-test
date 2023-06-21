d40ce3c9f5957a46efbc39e179a2ab21f381dfc4
Vitaly Samigullin <vrs@pilosus.org>
2023-06-21 18:52:47 +0200

    Initial commit

---
+++ test/org/pilosus/next_jdbc_test_test.clj
@@ -0,0 +1,7 @@
+(ns org.pilosus.next-jdbc-test-test
+  (:require [clojure.test :refer :all]
+            [org.pilosus.next-jdbc-test :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
